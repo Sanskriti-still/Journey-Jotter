8f669ed86bcad9f174dfba54787e1c22f3fac4b7
Sanskriti-still <sanskriti6334@gmail.com>
2026-08-14 19:10:21 +0530

    Fix TripPlanner path for deployment

--- frontend/src/pages/Planner/Planner.jsx
+++ frontend/src/pages/Planner/Planner.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import TripPlanner from "../../components/TripPlanner/TripPlanner";
+import TripPlanner from "../../components/TripPlannerComponent/TripPlanner";
 
 function Planner() {
   return (
